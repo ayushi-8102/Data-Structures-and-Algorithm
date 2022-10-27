654d4f78ced62b6ada2fb67e352c82ceae1a13c1
ayushi-8102 <ayushi.shukla8102@gmail.com>
2022-10-28 05:00:41 +0530

    new

---
+++ 12-PreComputation and Hashing/01-Precomutation_on_loops.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int M= 1e9+7;
+int N=1e5+10;
+
+
+int main() {
+  long long fact[N];
+  fact[0]=fact[1]=1;
+  for(int i=2;i<N;i++){
+    fact[i]=(fact[i-1]*i)%M;
+  }
+	int t;
+cin>>t;
+  while(t--){
+    int n;
+    cin>>n;
+   cout<< fact[n]<<endl;
+  }
+    
+}
